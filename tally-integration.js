ce9de94fa695630172a2e0b48e48c65798048c0d
carlaromo-tech <carla.romo@gmail.com>
2026-07-14 07:22:13 -0400

    Carga Tally correctamente antes de mostrar el formulario

--- tally-integration.js
+++ tally-integration.js
@@ -1,4 +1,5 @@
 const TALLY_FORM_ID = 'yPREXB';
+const TALLY_SCRIPT_URL = 'https://tally.so/widgets/embed.js';
 
 function getTestResultData() {
   const total = answers.reduce(function(sum, value) {
@@ -10,35 +11,3 @@ function getTestResultData() {
     result = 'Modo Supervisor activado';
   } else if (total > 30) {
     result = 'Comunicación interrumpida';
-  }
-
-  return {
-    puntaje: String(total),
-    resultado: result,
-    respuestas: answers.join('-'),
-    origen: 'test-vinculo-nido'
-  };
-}
-
-function requestLead() {
-  const data = getTestResultData();
-
-  if (!window.Tally || typeof window.Tally.openPopup !== 'function') {
-    window.location.href = 'https://tally.so/r/' + TALLY_FORM_ID +
-      '?puntaje=' + encodeURIComponent(data.puntaje) +
-      '&resultado=' + encodeURIComponent(data.resultado) +
-      '&respuestas=' + encodeURIComponent(data.respuestas) +
-      '&origen=' + encodeURIComponent(data.origen);
-    return;
-  }
-
-  window.Tally.openPopup(TALLY_FORM_ID, {
-    layout: 'modal',
-    width: 520,
-    hiddenFields: data,
-    onSubmit: function() {
-      window.Tally.closePopup(TALLY_FORM_ID);
-      showResult();
-    }
-  });
-}
